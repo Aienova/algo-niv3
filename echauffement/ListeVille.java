dfae9110226be3044eea92d895189783b2d2e0cb
Aienova <eudes.konda@aienova.com>
2025-06-12 12:30:40 +0200

    exo3

---
+++ echauffement/ListeVille.java
@@ -0,0 +1,56 @@
+package echauffement;
+
+
+/* A l'aide de boucle, tableau, exception et fonction. Me faire un tableau contenant 5 noms de pays (Vous pouvez choisir vos propres pays), créer une méthode pour afficher les pays du tableau numéroté par ligne et autre une méthode pour choisir un pays selon 
+
+la position de sa cellule du tableau et puis affichera un message de bienvenue dans la langue du pays choisi.:
+
+Bonjour, quelle ville voulez-vous visiter ?
+
+0 - Paris 
+1 - Londres
+2 - Madrid
+3 - Lisbonne
+4 - Berlin
+
+0
+Bienvenue à Paris !
+
+Voulez-vous visiter une autre ville ? (O/N)
+O
+
+1
+Welcome to London !
+
+Voulez-vous visiter une autre ville ? (O/N)
+O
+
+2
+Bienvenido a Madrid !
+Voulez-vous visiter une autre ville ? (O/N)
+O
+
+
+3
+Bem-vindo a Lisboa !
+Voulez-vous visiter une autre ville ? (O/N)
+O
+
+4
+Willkommen in Berlin !
+Voulez-vous visiter une autre ville ? (O/N)
+O
+
+
+5
+Désolé, cette ville n'est pas sur la liste !
+Voulez-vous visiter une autre ville ? (O/N)
+O
+
+Votre voyage est terminé :) !
+
+*/
+
+public class ListeVille {
+    
+}
